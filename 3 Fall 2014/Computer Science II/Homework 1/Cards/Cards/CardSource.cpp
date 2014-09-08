5fcda93cd812a038976d8c471fc592062f5f1f56
unknown <swiegand16@gmail.com>
2014-09-07 19:35:02 -0500

    Stuff Added

--- 3 Fall 2014/Computer Science II/Homework 1/Cards/Cards/CardSource.cpp
+++ 3 Fall 2014/Computer Science II/Homework 1/Cards/Cards/CardSource.cpp
@@ -7,9 +7,25 @@ using namespace std;
 enum Suit {heart, clubs, diamonds, spades};
 enum Face {Two, Three, Four, Five, Six, Seven, Eight,
 	Nine, Ten, Jack, Queen, King, Ace};
-int main ()
+struct Card
+{
+	Suit suit;
+	Face face;
+};
+typedef Card Deck[52];
+void fill_single (Card& card)
+{
+	cout<<"Please enter a suit and face value for a card.\n";
+		cin>>card.suit>>card.face;
+}
+void fill_all (Deck& deck)
 {
 
+}
+
+int main ()
+{
+	
 
 
 
