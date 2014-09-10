637bd6c271c417a3ece4213aada2ff9cdb10dee6
unknown <swiegand16@gmail.com>
2014-09-09 23:28:01 -0500

    Close Lab 2

---
+++ 3 Fall 2014/Computer Science II/Close Lab 2/CheckADT.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "CheckADT.h"
+using namespace std;
+void CheckADT::Open()
---
+++ 3 Fall 2014/Computer Science II/Close Lab 2/CheckADT.h
@@ -0,0 +1,21 @@
+/*Name:Sarah Wiegand
+  College:Southeast Missouri State University
+  Course Number and Section:CS 265-01
+  Date:09/10/14
+  Description:The purpose of this assignment is to create a class that stores account
+  information. The account holder should be able to deposit money into the account,
+  withdraw money from the account, write checks and check their current balance.
+  */
+#include <iostream>
+//#include <string>
+using namespace std;
+class CheckADT
+{
+public:
+
+private:
+	string name;
+	string ssNo;
+	int accountNo;
+	double balance;
+};
